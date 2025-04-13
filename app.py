d9cf4d79dd8cd3d0d63317cdfb36832ca91ce688
Amairi Omar <omar.amairi@etu.u-pec.fr>
2025-04-13 02:08:43 +0200

    Fin site Maison Haussmann

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, redirect, render_template, flash
+from email.message import EmailMessage
+from flask_cors import CORS
+import smtplib
+
+app = Flask(__name__, static_folder='assets')
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/projets")
+def projet():
+    return render_template("projet.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route('/contact', methods=["POST"])
+def mailDevis():
+    email = request.form.get("email")
+    phone = request.form.get("phone")
+    description = request.form.get("textarea")
+    
+    full_message = f"{description}\n\nEmail: {email}\nTéléphone: {phone}"
+
+    msg = EmailMessage()
+    msg.set_content(full_message)
+    msg["Subject"] = "Demande de devis"
+    msg["From"] = "demandedevis.maisonhaussmann@gmail.com"
+    msg["To"] = "mounioma@gmail.com"
+
+    s = smtplib.SMTP("smtp.gmail.com", 587)
+    s.starttls()
+    s.login("demandedevis.maisonhaussmann@gmail.com", "qgfwpleeotukozcz")
+    s.send_message(msg)
+    s.quit()
+
+    return redirect("/?success=1")
+
+if __name__ == '__main__':
+    app.run(debug=True)
